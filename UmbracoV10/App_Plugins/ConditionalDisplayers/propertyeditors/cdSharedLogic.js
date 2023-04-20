0a0f3a4e900b3077a870e86f0dddec8a48cb1a61
Mario Lopez <mario@luminary.com>
2023-04-20 17:29:27 +1000

    modify icons and names

--- UmbracoV10/App_Plugins/ConditionalDisplayers/propertyeditors/cdSharedLogic.js
+++ UmbracoV10/App_Plugins/ConditionalDisplayers/propertyeditors/cdSharedLogic.js
@@ -6,14 +6,27 @@ angular.module('cd.resources').factory('cdSharedLogic', cdSharedLogic);
 function cdSharedLogic() {
     return {
         //the properties with alias in 'show' and 'hide' will be affected when the value is triggered.
-        displayProps: function (show, hide, parentPropertyAlias) {
+        displayProps: function (show, hide, parentPropertyAlias, parentBlockListItemId) {
             //Elements to show
             if (show) {
                 var showEls = show.split(',');
 
                 if (showEls && showEls.length > 0) {
                     var s = elSelectors(showEls, parentPropertyAlias);
-                    $(s).show("slow");
+                    $(s).each(function () {
+
+                        // Checking if we have parent-id.
+                        if (parentBlockListItemId) {
+                            let parent = $(this).closest(`[data-cd-ancestor-id=${parentBlockListItemId}]`)[0];
+
+                            // Checking whether the element belongs to the parent or not.
+                            if (parent) {
+                                $(this).show("slow");
+                            }
+                        } else {
+                            $(this).show("slow");
+                        }
+                    });
                 }
             }
 
@@ -23,7 +36,20 @@ function cdSharedLogic() {
 
                 if (hideEls && hideEls.length > 0) {
                     var h = elSelectors(hideEls, parentPropertyAlias);
-                    $(h).hide("slow");
+                    $(h).each(function () {
+
+                        // Checking if we have parent-id.
+                        if (parentBlockListItemId) {
+                            let parent = $(this).closest(`[data-cd-ancestor-id=${parentBlockListItemId}]`)[0];
+
+                            // Checking whether the element belongs to the parent or not.
+                            if (parent) {
+                                $(this).hide("slow");
+                            }
+                        } else {
+                            $(this).hide("slow");
+                        }
+                    });
                 }
             }
 
@@ -48,6 +74,6 @@ function cdSharedLogic() {
 
                 return h;
             }
-        }        
+        }
     }
 }
--- UmbracoV10/App_Plugins/ConditionalDisplayers/propertyeditors/checkbox/checkbox.controller.js
+++ UmbracoV10/App_Plugins/ConditionalDisplayers/propertyeditors/checkbox/checkbox.controller.js
@@ -1,7 +1,17 @@
 ﻿angular.module("umbraco").controller("Our.Umbraco.ConditionalDisplayers.CheckboxController", cdCheckboxController);
 
 
-function cdCheckboxController($scope, editorState, cdSharedLogic) {
+function cdCheckboxController($scope, $element, editorState, cdSharedLogic) {
+    // Getting the parent umbraco element.
+    let parentBlockListBlock = $element[0].closest('umb-block-list-block');
+    let parentBlockListItemId = undefined;
+
+    // Setting data-cd-ancestor-id if checkbox placed into blockListItem
+    // to hide or show only child elements of it.
+    if (parentBlockListBlock) {
+        parentBlockListItemId = `checkbox-cd-ancestor-id-${$scope.$id}`;
+        parentBlockListBlock.setAttribute('data-cd-ancestor-id', parentBlockListItemId);
+    }
 
     // propertyAlias is used in NestedContent properties. If we find we are in NC we
     // extract the parent alias to find later on only the property belonging to the same item where CD is included.
@@ -9,17 +19,27 @@ function cdCheckboxController($scope, editorState, cdSharedLogic) {
         var parentPropertyAlias = $scope.model.alias.slice(0, -$scope.model.propertyAlias.length);
     }
 
+    // Define prevalues for labelOff and labelOn
+    $scope.labelOff = $scope.model.config.labelOff;
+    $scope.labelOn = $scope.model.config.labelOn;
+
+    // Set initial toggle label and visibility based on the showLabels config option
+    $scope.toggleLabel = $scope.model.config.showLabels ? $scope.labelOff : "";
+    $scope.showLabels = $scope.model.config.showLabels;
+
     $scope.clicked = function () {
         $scope.renderModel.value = !$scope.renderModel.value;
+        // Update the toggle label based on the new value
+        $scope.toggleLabel = $scope.showLabels ? ($scope.renderModel.value ? $scope.labelOn : $scope.labelOff) : "";
     };
 
     $scope.runDisplayLogic = function () {
         if (editorState.current.ModelState) {
             //init visible fields
             if ($scope.renderModel.value) {
-                cdSharedLogic.displayProps($scope.model.config.showIfChecked, $scope.model.config.showIfUnchecked, parentPropertyAlias);
+                cdSharedLogic.displayProps($scope.model.config.showIfChecked, $scope.model.config.showIfUnchecked, parentPropertyAlias, parentBlockListItemId);
             } else {
-                cdSharedLogic.displayProps($scope.model.config.showIfUnchecked, $scope.model.config.showIfChecked, parentPropertyAlias);
+                cdSharedLogic.displayProps($scope.model.config.showIfUnchecked, $scope.model.config.showIfChecked, parentPropertyAlias, parentBlockListItemId);
             }
         }
     };
